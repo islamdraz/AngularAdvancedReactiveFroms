cc2b4ef19363e43f1e11a23fcaa452c899b695d4
islam.draz <eng.islamdraz@gmail.com>
2020-06-09 18:27:32 +0200

    adding the reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { CustomerComponent } from './customers/customer.component';
+import { AppComponent } from "./app.component";
+import { CustomerComponent } from "./customers/customer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
+  declarations: [AppComponent, CustomerComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    // FormsModule
+    ReactiveFormsModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
