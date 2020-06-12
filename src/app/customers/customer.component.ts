d1975f672a175baf395a80477cba0a0a1ae75fec
islam.draz <eng.islamdraz@gmail.com>
2020-06-12 11:08:15 +0200

    add validation throughout the formGroup

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { debounceTime } from "rxjs/operators";
 import {
   FormGroup,
-  FormControl,
+  // FormControl,
   FormBuilder,
   Validators,
   AbstractControl,
@@ -10,7 +11,19 @@ import {
 
 import { Customer } from "./customer";
 
-function ratingRang(min: number, max: number): ValidatorFn{
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  var mail = c.get("email");
+  let confirmMail = c.get("confirmEmail");
+  if (mail.pristine || confirmMail.pristine) {
+    return null;
+  }
+  if (mail.value === confirmMail.value) return null;
+  else {
+    return { match: true };
+  }
+}
+
+function ratingRang(min: number, max: number): ValidatorFn {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
     if (
       control.value != null &&
@@ -19,7 +32,7 @@ function ratingRang(min: number, max: number): ValidatorFn{
       return { rating: true };
     }
     return null;
-  }
+  };
 }
 
 @Component({
@@ -31,6 +44,11 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
 
+  emailErrorMessage;
+  private validationMessages = {
+    required: "please enter email address",
+    email: "please enter valide email address",
+  };
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
@@ -44,13 +62,29 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ["", [Validators.required, Validators.minLength(3)]],
       lastName: ["", [Validators.required, Validators.maxLength(50)]],
-      email: ["", [Validators.required, Validators.email]],
+      emailGroup: this.fb.group(
+        {
+          email: ["", [Validators.required, Validators.email]],
+          confirmEmail: ["", [Validators.required]],
+        },
+        { validator: emailMatcher }
+      ),
       phone: ["", []],
       notification: "email",
-      rating: [null, [ratingRang(0,5)]],
+      rating: [null, [ratingRang(0, 5)]],
       // sendCatalog: [{ value: true, disabled: false }],
       sendCatalog: true,
     });
+
+    const notificationCtrl = this.customerForm.get("notification");
+    notificationCtrl.valueChanges.subscribe((value) =>
+      this.setNotification(value)
+    );
+    //this has an issue as you don't get notified when the use change the focuse without type anything
+    const emailControl = this.customerForm.get("emailGroup.email");
+    emailControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe((value) => this.setMessage(emailControl));
   }
 
   setNotification(value: string): void {
@@ -62,6 +96,17 @@ export class CustomerComponent implements OnInit {
     }
 
     phoneCtrl.updateValueAndValidity();
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailErrorMessage = "";
+
+    console.log(c.touched, c.dirty, c.errors);
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailErrorMessage = Object.keys(c.errors)
+        .map((key) => this.validationMessages[key])
+        .join(" ");
+    }
   }
 
   save() {
